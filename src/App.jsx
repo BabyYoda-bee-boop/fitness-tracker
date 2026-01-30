b25706552e760291885a0d85185b75918bef0207
VR-dev <182447330+BabyYoda-bee-boop@users.noreply.github.com>
2026-01-30 17:27:53 +0100

    add content to section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import DisplayCard from "./Components/DisplayCard/DisplayCard";
 import Logo from "./Components/Logo/Logo";
 import fireIcon from "./assets/fireIcon.svg";
+import zigArrowIcon from "./assets/zigArrowIcon.svg";
+import bullseyeIcon from "./assets/bullseyeIcon.svg";
 
 function App() {
   return (
@@ -9,11 +11,26 @@ function App() {
         <Logo color="#b3cfb3;" />
       </header>
       <main>
-        <DisplayCard
-          title="Today's workouts"
-          details="Keep going!"
-          icon_src={fireIcon}
-        />
+        <section className="flex--overview">
+          <DisplayCard
+            title="Today's workouts"
+            details="Keep going!"
+            variant="fire"
+            icon_src={fireIcon}
+          />
+          <DisplayCard
+            title="Weekly streak"
+            details="TODO: Keep going!"
+            variant="zigArrow"
+            icon_src={zigArrowIcon}
+          />
+          <DisplayCard
+            title="Weekly goal"
+            details="TODO: days in a row"
+            variant="bullseye"
+            icon_src={bullseyeIcon}
+          />
+        </section>
       </main>
     </div>
   );
